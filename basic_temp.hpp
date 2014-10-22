52620c6911f5b1ef65735f529487bbd178fabde1
XuyangKang <xuyangkang@gmail.com>
2014-10-22 22:56:34 +0800

    Use cstdint header

--- basic_temp.hpp
+++ basic_temp.hpp
@@ -2,20 +2,22 @@
 #define __ALGO_BASIC_TEMP__
 
 /*
-我是一个MC男孩站在DJ台
-手里拿着麦克风我感觉很气派
-现在我要你跟我一起来摇摆
-这里是来自情债的新一代
-*/
+ * 我是一个MC男孩站在DJ台
+ * 手里拿着麦克风我感觉很气派
+ * 现在我要你跟我一起来摇摆
+ * 这里是来自情债的新一代
+ */
 
 #include <algorithm>
 #include <cassert>
 #include <cctype>
 #include <cmath>
 #include <cstdio>
+#include <cstdint>
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
+#include <fstream>
 #include <map>
 #include <set>
 #include <stack>
@@ -34,14 +36,5 @@ using namespace std;
 #else
 #define debug(x) ;
 #endif 
-
-// type define
-typedef char int8;
-typedef unsigned char uint8;
-typedef short int16;
-typedef unsigned short uint16;
-typedef unsigned int uint;
-typedef long long int64;
-typedef unsigned long long uint64;
 
 #endif
