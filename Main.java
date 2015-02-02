cf6696c6c2c01c75931e612e9faf904e7f38ce0f
XuyangKang <xuyangkang@gmail.com>
2015-02-02 22:58:34 +0800

    BinaryIndexedTree

--- Main.java
+++ Main.java
@@ -10,7 +10,6 @@ import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -20,159 +19,64 @@ public class Main implements Runnable {
   private static final String INPUT_FILE = "";
 
   private void solve(QuickScanner in, PrintWriter out) {
-    out.println("Hello World!");
+    out.print("Done");
   }
 
-  private class SegmentTree {
-    class TreeNode {
-      int l;
-      int r;
-      TreeNode lc;
-      TreeNode rc;
+  private class BinaryIndexedTree {
+    int length;
+    int logLength;
+    int[] c;
 
-      // Add data field below.
+    BinaryIndexedTree(int n) {
+      logLength = Integer.SIZE - Integer.numberOfLeadingZeros(n);
+      length = 1 << logLength;
+      c = new int[length + 1];
+    }
 
-      // Implement these according to problems. May add args.
-      TreeNode pushUp() {
-        throw new AssertionError("Push up is not implemented.");
-      }
-
-      TreeNode pushDown() {
-        if (lc == null) {
-          this.split();
-        }
-        throw new AssertionError("Push down is not implemented.");
-      }
-
-      TreeNode update() {
-        throw new AssertionError("Update is not implemented.");
-      }
-
-      TreeNode() {
-        this(0, 0, null, null);
-      }
-
-      TreeNode(int l, int r) {
-        this(l, r, null, null);
-      }
-
-      TreeNode(int l, int r, TreeNode lc, TreeNode rc) {
-        this.l = l;
-        this.r = r;
-        this.lc = lc;
-        this.rc = rc;
-      }
-
-      int getMid() {
-        return (l + r) >> 1;
-      }
-
-      boolean isLeaf() {
-        return l == r;
-      }
-
-      TreeNode split() {
-        if (l != r) {
-          int mid = getMid();
-          lc = new TreeNode(l, mid);
-          rc = new TreeNode(mid + 1, r);
-        }
-        return this;
-      }
-
-      TreeNode expand() {
-        split();
-        if (!isLeaf()) {
-          lc.expand();
-          rc.expand();
-        }
-        return this;
+    void add(int x, int num) {
+      for (; x <= length; x += Integer.lowestOneBit(x)) {
+        c[x] += num;
       }
     }
 
-    TreeNode root;
-
-    SegmentTree(int l, int r) {
-      this(l, r, false);
+    void insert(int x) {
+      add(x, 1);
     }
 
-    SegmentTree(int l, int r, boolean dynamicExpand) {
-      root = new TreeNode(l, r);
-      if (!dynamicExpand) {
-        root.expand();
+    void remove(int x) {
+      add(x, -1);
+    }
+
+    int sum(int x) {
+      int sum = 0;
+      for (; x > 0; x -= Integer.lowestOneBit(x)) {
+        sum += c[x];
       }
+      return sum;
     }
 
-    void updateKey(int key) {
-      updateKeyWorker(root, key);
+    int sum(int l, int r) {
+      return sum(r) - sum(l - 1);
     }
 
-    private void updateKeyWorker(TreeNode now, int key) {
-      // now.update();
-      // now.pushDown();
-      if (!now.isLeaf()) {
-        if (key <= now.getMid()) {
-          updateKeyWorker(now.lc, key);
+    int rank(int x) {
+      return sum(x - 1) + 1;
+    }
+
+    int select(int k) {
+      int ans = 0;
+      for (int i = logLength; i >= 0; i--) {
+        ans += (1 << i);
+        if (ans <= length && c[ans] < k) {
+          k -= c[ans];
         } else {
-          updateKeyWorker(now.rc, key);
+          ans -= (1 << i);
         }
-        // now.pushUp();
       }
-    }
-
-    void updateRange(int l, int r) {
-      updateRangeWorker(root, l, r);
-    }
-
-    private void updateRangeWorker(TreeNode now, int l, int r) {
-      if (r < now.l || l > now.r) {
-        return;
-      }
-      if (l <= now.l && now.r <= r) {
-        // now.update();
-      } else {
-        // now.pushDown();
-        updateRangeWorker(now.lc, l, r);
-        updateRangeWorker(now.rc, l, r);
-        // now.pushUp();
-      }
-    }
-
-    void queryKey(int key) {
-      queryKeyWorker(root, key);
-    }
-
-    private void queryKeyWorker(TreeNode now, int key) {
-      if (!now.isLeaf()) {
-        // now.pushDown();
-        if (key <= now.getMid()) {
-          queryKeyWorker(now.lc, key);
-        } else {
-          queryKeyWorker(now.lc, key);
-        }
-        // now.pushUp();
-      }
-    }
-
-    void queryRange(int l, int r) {
-      queryRangeWorker(root, l, r);
-    }
-
-    private void queryRangeWorker(TreeNode now, int l, int r) {
-      if (r < now.l || l > now.r) {
-        return;
-      }
-      if (l <= now.r && now.r <= r) {
-        return;
-      } else {
-        // now.pushDown();
-        queryRangeWorker(now.lc, l, r);
-        queryRangeWorker(now.rc, l, r);
-        // now.pushUp();
-      }
+      return ans + 1;
     }
   }
-
+  
   private static class MathUtils {
 
     private MathUtils() {
@@ -230,91 +134,6 @@ public class Main implements Runnable {
     }
   }
 
-  private class LessComparator<T extends Comparable<T>> implements Comparator<T> {
-    @Override
-    public int compare(T a, T b) {
-      return a.compareTo(b);
-    }
-  }
-
-  private class GreaterComparator<T extends Comparable<T>> implements Comparator<T> {
-    @Override
-    public int compare(T a, T b) {
-      return b.compareTo(a);
-    }
-  }
-
-  /**
-   * A class to answer Range Minimum/Maximum Query problem.
-   *
-   */
-  private class RMQ<T extends Comparable<T>> {
-
-    private final T[] data;
-    private final int length;
-    private final int logLength;
-    private final int[][] table;
-    private final Comparator<T> comparator;
-
-    /**
-     * Build Sparse Table with comparator.
-     * 
-     * @param data the origin data.
-     * @param comparator how to compare the data.
-     */
-    RMQ(T[] data, Comparator<T> comparator) {
-      this.data = data;
-      this.comparator = comparator;
-      length = data.length;
-      logLength = log2(length) + 1;
-      table = new int[logLength][length];
-      for (int i = 0; i < length; i++) {
-        table[0][i] = i;
-      }
-      for (int i = 1, k = 1; i < logLength; i++, k <<= 1) {
-        int[] now = table[i - 1];
-        int[] next = table[i];
-        for (int j = 0; j + k < length; j++) {
-          int tmp1 = now[j];
-          int tmp2 = now[j + k];
-          if (comparator.compare(data[tmp1], data[tmp2]) <= 0) {
-            next[j] = tmp1;
-          } else {
-            next[j] = tmp2;
-          }
-        }
-      }
-    }
-
-    /**
-     * Query and return the index of the "smallest" number defined by comparator in range[from, to]
-     * 
-     * @param from the left border of the range, inclusive.
-     * @param to the right border of the range, inclusive.
-     * @return the index of the "smallest" number.
-     */
-    int query(int from, int to) {
-      to++;
-      int k = log2(to - from);
-      int tmp1 = table[k][from];
-      int tmp2 = table[k][to - (1 << k)];
-      if (comparator.compare(data[tmp1], data[tmp2]) <= 0) {
-        return tmp1;
-      } else {
-        return tmp2;
-      }
-    }
-
-    T get(int index) {
-      return data[index];
-    }
-
-    private int log2(int b) {
-      return 31 - Integer.numberOfLeadingZeros(b);
-    }
-  }
-
-  // Tedious code.
   public Main() {}
 
   private class QuickScanner {
@@ -327,7 +146,6 @@ public class Main implements Runnable {
     }
 
     boolean hasNext() {
-      // Try to get next and put it into nextHolder.
       if (nextHolder == null) {
         nextHolder = next();
       }
@@ -335,7 +153,6 @@ public class Main implements Runnable {
     }
 
     String next() {
-      // If called hasNext before, should return string in nextHolder.
       if (nextHolder != null) {
         String next = nextHolder;
         nextHolder = null;
@@ -413,9 +230,9 @@ public class Main implements Runnable {
 
   @Override
   public void run() {
+    Reader reader = null;
     PrintWriter out = new PrintWriter(System.out);
     try {
-      Reader reader = null;
       if (INPUT_FILE.isEmpty()) {
         reader = new InputStreamReader(System.in);
       } else {
@@ -427,7 +244,6 @@ public class Main implements Runnable {
     } catch (Exception e) {
       e.printStackTrace();
     } finally {
-      // To make sure PrintWriter will always flush to stdout.
       out.flush();
     }
   }
