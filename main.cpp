6bde31f8deb72da53017ec7561bba0bf73191006
XuyangKang <xuyangkang@gmail.com>
2015-02-11 22:31:17 +0800

    Add basic C++ template

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <cassert>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+using int8 = char;
+using int16= short;
+using int32 = int;
+using int64 = long long;
+using uint8 = unsigned char;
+using uint16 = unsigned short;
+using uint32 = unsigned int;
+using uint64 = unsigned long long;
+using byte = unsigned char;
+
+#ifdef LOCAL_DEBUG
+#define debug(x) cerr << #x << " = " << x << endl;
+#else
+#define debug(x) ;
+#endif
+
+int main() {
+  cout << "Hello World!" << endl;
+  return 0;
+}
