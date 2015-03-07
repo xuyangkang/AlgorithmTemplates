c09027ef026795b038ddbf08153bb58f9410b369
XuyangKang <xuyangkang@gmail.com>
2015-03-07 23:27:12 +0800

    Add a workspace for cpp.

---
+++ workshop.cpp
@@ -0,0 +1,61 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <fstream>
+#include <functional>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+using int8 = char;
+using int16= short;
+using int32 = int;
+using int64 = long long;
+using uint8 = unsigned char;
+using uint16 = unsigned short;
+using uint32 = unsigned int;
+using uint64 = unsigned long long;
+using byte = unsigned char;
+
+const double PI = acos(0.0) * 2.0;
+const double EPS = 1e-10;
+const int MOD = 1E9 + 7;
+
+#ifdef LOCAL_DEBUG
+#define debug(x) cerr << #x << " = " << x << endl
+#else
+#define debug(x) ;
+#endif
+
+#include "cpp_libs/bits.hpp"
+
+struct StaticRunner {
+  StaticRunner() {
+    ios_base::sync_with_stdio(0);
+  }
+} static_runner;
+
+int main() {
+  cout << "Hello World!" << endl;
+  return 0;
+}
